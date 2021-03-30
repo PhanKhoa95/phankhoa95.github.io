46e35f0b437351a5ac5d5d6152f934bdea154bbe
PhanKhoa95 <62231387+PhanKhoa95@users.noreply.github.com>
2021-03-30 16:57:06 +0700

    pug
    on

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,7 +185,42 @@ $('.nav__dropdown-info').on('click', (e) => {
     $(`#${parentId} > .nav__dropdown-box`).toggleClass('shown');
 });
 
+// ////////////////////////////////////// time
+function animateNumber(finalNumber, duration = 5000, startNumber = 0, callback) {
+  let currentNumber = startNumber
+  const interval = window.setInterval(updateNumber, 17)
+  function updateNumber() {
+    if (currentNumber >= finalNumber) {
+      clearInterval(interval)
+    } else {
+      let inc = Math.ceil(finalNumber / (duration / 17))
+      if (currentNumber + inc > finalNumber) {
+        currentNumber = finalNumber
+        clearInterval(interval)
+      } else {
+        currentNumber += inc
+      }
+      callback(currentNumber)
+    }
+  }
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+  animateNumber(5000000, 30000, 0, function (number) {
+    const formattedNumber = number.toLocaleString()
+    document.getElementById('transaction-count').innerText = formattedNumber
+  })
+  
+  animateNumber(50000, 25000, 0, function (number) {
+    const formattedNumber = number.toLocaleString()
+    document.getElementById('city-count').innerText = formattedNumber
+  })
+  
+  // animateNumber(1500, 3000, 0, function (number) {
+  //   const formattedNumber = number.toLocaleString()
+  //   document.getElementById('customer-count').innerText = formattedNumber
+  // })
+})
 
 
 
